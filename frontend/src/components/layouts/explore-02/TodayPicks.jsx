d9d0bf994c05234619dda45d734b5ced6c4ee236
kfbustam <karoljosefb@gmail.com>
2022-12-15 23:50:52 -0500

    refactor: Add actual data

--- frontend/src/components/layouts/explore-02/TodayPicks.jsx
+++ frontend/src/components/layouts/explore-02/TodayPicks.jsx
@@ -7,331 +7,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import { Link, useNavigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
-import img1 from '../../../assets/images/box-item/card-item-3.jpg'
-import imga1 from '../../../assets/images/avatar/avt-1.jpg'
-import imgCollection1 from '../../../assets/images/avatar/avt-18.jpg'
-import img2 from '../../../assets/images/box-item/card-item-4.jpg'
-import imga2 from '../../../assets/images/avatar/avt-2.jpg'
-import imgCollection2 from '../../../assets/images/avatar/avt-18.jpg'
-import img3 from '../../../assets/images/box-item/card-item-2.jpg'
-import imga3 from '../../../assets/images/avatar/avt-4.jpg'
-import imgCollection3 from '../../../assets/images/avatar/avt-18.jpg'
-import img4 from '../../../assets/images/box-item/card-item-7.jpg'
-import imga4 from '../../../assets/images/avatar/avt-3.jpg'
-import imgCollection4 from '../../../assets/images/avatar/avt-18.jpg'
-import img5 from '../../../assets/images/box-item/card-item8.jpg'
-import imga5 from '../../../assets/images/avatar/avt-12.jpg'
-import imgCollection5 from '../../../assets/images/avatar/avt-18.jpg'
-import img6 from '../../../assets/images/box-item/card-item-9.jpg'
-import imga6 from '../../../assets/images/avatar/avt-1.jpg'
-import imgCollection6 from '../../../assets/images/avatar/avt-18.jpg'
-import img7 from '../../../assets/images/box-item/image-box-6.jpg'
-import imga7 from '../../../assets/images/avatar/avt-4.jpg'
-import imgCollection7 from '../../../assets/images/avatar/avt-18.jpg'
-import img8 from '../../../assets/images/box-item/image-box-11.jpg'
-import imga8 from '../../../assets/images/avatar/avt-3.jpg'
-import imgCollection8 from '../../../assets/images/avatar/avt-18.jpg'
-import img12 from '../../../alice_video.png'
-
 const LISTINGS_URL = "http://localhost:8080/nft/listings"
+const IMAGE_BASE_URL = "http://localhost:8080/images?image_name=";
 
-const TodayPicks = () => {
+const TodayPicks = ({ dataPanel }) => {
     const token = "test123";
 
-    const [dataPanel, setDataPanel] = useState(
-        [
-            {
-                id: 1,
-                dataContent: [
-                    {
-                        id: 1,
-                        img: img12,
-                        title: "Alice’s Best Picture",
-                        tags: "bsc",
-                        imgAuthor: imga1,
-                        nameAuthor: "alice",
-                        price: "10 BTC",
-                        priceChange: "",
-                        wishlist: "1",
-                        imgCollection: imgCollection1,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 2,
-                        img: img12,
-                        title: "Alice’s Best Video",
-                        tags: "bsc",
-                        imgAuthor: imga2,
-                        nameAuthor: "alice",
-                        price: "100 BTC",
-                        priceChange: "",
-                        wishlist: "1",
-                        imgCollection: imgCollection2,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "all"
-                    },
-                    {
-                        id: 3,
-                        img: img3,
-                        title: "Event Wide NFT ",
-                        tags: "bsc",
-                        imgAuthor: imga3,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection3,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "all"
-                    },
-                    {
-                        id: 4,
-                        img: img4,
-                        title: "Pokemon NFT ",
-                        tags: "bsc",
-                        imgAuthor: imga4,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection4,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 5,
-                        img: img5,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga5,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection5,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 6,
-                        img: img6,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga6,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection6,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "auction"
-                    },
-                    {
-                        id: 7,
-                        img: img7,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga7,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection7,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "auction"
-                    },
-                    {
-                        id: 8,
-                        img: img8,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga8,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection8,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "all"
-                    },
-                    {
-                        id: 9,
-                        img: img1,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga1,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection1,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "all"
-                    },
-                    {
-                        id: 10,
-                        img: img2,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga2,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection2,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "auction"
-                    },
-                    {
-                        id: 11,
-                        img: img3,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga3,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection3,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "auction"
-                    },
-                    {
-                        id: 12,
-                        img: img4,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga4,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection4,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 13,
-                        img: img5,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga5,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection5,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "auction"
-                    },
-                    {
-                        id: 14,
-                        img: img6,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga6,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection6,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 15,
-                        img: img7,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga7,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection7,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 16,
-                        img: img8,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga8,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection8,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "all"
-                    },
-                ]
-            },
-            {
-                id: 2,
-                dataContent: [
-                    {
-                        id: 2,
-                        img: img2,
-                        title: "The TESTED Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga2,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection2,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 3,
-                        img: img3,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga3,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection3,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                    {
-                        id: 4,
-                        img: img4,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga4,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection4,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "auction"
-                    },
-                    {
-                        id: 5,
-                        img: img5,
-                        title: "The RenaiXance Rising the sun ",
-                        tags: "bsc",
-                        imgAuthor: imga5,
-                        nameAuthor: "SalvadorDali",
-                        price: "4.89 ETH",
-                        priceChange: "$12.246",
-                        wishlist: "100",
-                        imgCollection: imgCollection5,
-                        nameCollection: "Creative Art 3D",
-                        saleType: "immediate"
-                    },
-                ]
-            }
-        ]
-    )
 
     useEffect(() => {
         // fetch(LISTINGS_URL + "?token=" + token, {
@@ -373,14 +54,13 @@ const TodayPicks = () => {
         //navigate("/wallet-connect");
 
     }
-    
 
 
     const [modalShow, setModalShow] = useState(false);
 
     return (
         <Fragment>
-                        <ToastContainer theme="dark" position="top-center" />
+            <ToastContainer theme="dark" position="top-center" />
 
             <div className="tf-section sc-explore-2">
                 <div className="themesflat-container">
@@ -423,17 +103,17 @@ const TodayPicks = () => {
                                                 {
 
                                                     data.dataContent.slice(0, visible).map(item => (
-                                                        <div key={item.id} className={`sc-card-product explode style2 mg-bt ${item.feature ? 'comingsoon' : ''} `}>
+                                                        <div key={item.id} className={`sc-card-product explode style2 mg-bt ${item.nftType} `}>
                                                             <div className="card-media">
-                                                                <Link to="/item-details-01"><img src={item.img} alt="Axies" state={{ item }} /></Link>
+                                                                <Link to="/item-details-01"><img src={IMAGE_BASE_URL + item.imageUrl} alt="Axies" state={{ item }} /></Link>
                                                                 <div className="button-place-bid">
                                                                     <button onClick={() => buyNowFunc()} className="sc-button style-place-bid style bag fl-button pri-3"><span>Purchase</span></button>
                                                                 </div>
-                                                                <Link to="/login" className="wishlist-button heart"><span className="number-like">{item.wishlist}</span></Link>
-                                                                <div className="coming-soon">{item.feature}</div>
+                                                                <Link to="/login" className="wishlist-button heart"><span className="number-like">{item.lastRecordedTime}</span></Link>
+                                                                <div className="coming-soon">{item.nftType}</div>
                                                             </div>
                                                             <div className="card-title">
-                                                                <h5><Link to="/item-details-01" state={{ item }}>"{item.title}"</Link></h5>
+                                                                <h5><Link to="/item-details-01" state={{ item }}>"{item.name}"</Link></h5>
 
                                                             </div>
                                                             <div className="meta-info">
@@ -457,7 +137,7 @@ const TodayPicks = () => {
                                                                             <span>Current Price</span>
                                                                             <div className="price-details">
                                                                                 <h5>{item.price}</h5>
-                                                                                <span>= {item.priceChange}</span>
+                                                                                <span>= {item.smartContractAddress}</span>
                                                                             </div>
                                                                         </>
 
@@ -469,7 +149,7 @@ const TodayPicks = () => {
                                                                             <span>Current Bid</span>
                                                                             <div className="price-details">
                                                                                 <h5>{item.price}</h5>
-                                                                                <span>= {item.priceChange}</span>
+                                                                                <span>= {item.smartContractAddress}</span>
                                                                             </div>
                                                                         </>
                                                                     }
--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
@@ -11,7 +11,7 @@ const GET_ALL_NFTS = "http://localhost:8080/nft"
 
 const Browse = () => {
     const [assetCollection, setAssetCollection] = React.useState([]);
-
+    const [dataPanel, setDataPanel] = useState([{ id: 1, dataContent: [] }]);
     React.useEffect(() => {
         fetch(
             GET_ALL_NFTS,
@@ -23,23 +23,24 @@ const Browse = () => {
                 },
                 mode: 'cors'
             }
-        ).then( response => {
+        ).then(response => {
             if (response.ok) {
                 return response.json();
             }
             throw response
         })
-        .then( collection => {
-            console.log("remote collection", collection);
-        })
+            .then(collection => {
+                setDataPanel([{ id: 1, dataContent: collection }]);
+                console.log("remote collection", dataPanel);
+            })
     }, [])
 
 
     return (
         <div className='explore'>
             <Header />
-            <PathBanner heading="Marketplace"/>
-            <TodayPicks data={todayPickData} />
+            <PathBanner heading="Marketplace" />
+            <TodayPicks dataPanel={dataPanel} />
             <Footer />
         </div>
     );
