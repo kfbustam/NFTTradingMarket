a64748f59fb60e9de589c0f1a7056317f8752a2e
sjsudev <devansh.modi@sjsu.edu>
2022-12-15 16:31:27 -0800

    add get transaction endpoint

--- initial/src/main/java/com/example/restservice/controller/NFTTradingMarketCryptoController.java
+++ initial/src/main/java/com/example/restservice/controller/NFTTradingMarketCryptoController.java
@@ -4,6 +4,7 @@ import com.example.restservice.*;
 import com.example.restservice.crypto.CryptoType;
 import com.example.restservice.nft.NFT;
 import com.example.restservice.nft.NftService;
+import com.example.restservice.nft.NftTransaction;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * The type Airline reservation system rest controller.
@@ -140,6 +142,43 @@ public class NFTTradingMarketCryptoController {
             ex.printStackTrace(pw);
             System.out.println(sw.toString());
             return new ResponseEntity<String>("{\"BadRequest\": {\"code\": \" 400 \",\"msg\": " + ex.getMessage() +"}}", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/transactions")
+    @ResponseBody
+    public ResponseEntity<List<NftTransaction>> getTransactions(
+            @RequestParam(name="token", required=true) @NotNull String token
+    ) {
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        try {
+
+            Optional<SessionToken> optionalSession = service.getSessionByToken(token);
+
+            if (optionalSession.isEmpty()) {
+                return new ResponseEntity<List<NftTransaction>>(List.of(), HttpStatus.UNAUTHORIZED);
+            }
+
+            User user = service.getSessionByToken(token).orElseThrow().getUser();
+            List<NftTransaction> transactions = nftService.getTransactions(user)
+                            .stream().collect(Collectors.toList());
+
+
+            ResponseEntity<List<NftTransaction>> res = new ResponseEntity<List<NftTransaction>>(
+                    transactions,
+                    responseHeaders,
+                    200
+            );
+
+            return res;
+        } catch (Exception ex) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            System.out.println(sw.toString());
+            return new ResponseEntity<List<NftTransaction>>(List.of(), HttpStatus.BAD_REQUEST);
         }
     }
 }
---
+++ initial/src/main/java/com/example/restservice/nft/NftTransaction.java
@@ -0,0 +1,103 @@
+package com.example.restservice.nft;
+
+import com.example.restservice.NftUserType;
+import com.example.restservice.User;
+import com.example.restservice.crypto.CryptoType;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="transactions")
+public class NftTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(targetEntity = NFT.class)
+    @JoinColumn(name = "nft_id")
+    private NFT nft;
+
+    @OneToOne(targetEntity = User.class)
+    @JoinColumn(name = "seller_id")
+    private User seller;
+
+    @OneToOne(targetEntity = User.class)
+    @JoinColumn(name = "buyer_id")
+    private User buyer;
+
+    @Column(name="status", columnDefinition="VARCHAR(50) default 'Completed'")
+    private String status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "currency")
+    private CryptoType type;
+
+    @Column(name = "timestamp")
+    private Date date;
+
+    @Column(name="amount", columnDefinition="Decimal(10,2) default '0.00'")
+    private BigDecimal amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public NFT getNft() {
+        return nft;
+    }
+
+    public void setNft(NFT nft) {
+        this.nft = nft;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public CryptoType getType() {
+        return type;
+    }
+
+    public void setType(CryptoType type) {
+        this.type = type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
